47e9dac7f793bb918b0844169857c16ea698cb84
George Gikas <gcgikas@gmail.com>
2015-07-06 14:13:48 -0500

    Initial Commit

---
+++ College Profile Builder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  College Profile Builder
+//
+//  Created by George Gikas on 7/6/15.
+//  Copyright © 2015 George Gikas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
